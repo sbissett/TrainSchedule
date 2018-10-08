8433ad1dcb3ac4f11c68dc0d76f4819014b0705d
Stephen Bissett <steve.bissett5@gmail.com>
2018-10-08 10:42:04 -0600

    MondayPush

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,86 +1,128 @@
-//Initialize Firebase
-var config = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectld: "",
-    storageBucket: "",
-    messagingSenderId: ""
 
-};
 
-firebase.intializeA;; (config);
+$(document).ready(function() {
 
-//Create variable to reference the database
-
-var database = firebase.database();
-
-//Run Time
-
-setInterval(function (startTime) {
-    $("#time").html(moment().format('hh:mm a'))
-}, 1000);
-
-// Capture Button Click
-$("#add-train").on("click", function () {
-    // Don't refresh the page!
-    event.preventDefault();
-
-    // Code in the logic for storing and retrieving the most recent train information
-    var train = $("#trainname-input").val().trim();
-    var destination = $("#destination-input").val().trim();
-    var frequency = $("#frequency-input").val().trim();
-    var firstTime = $("#firsttime-input").val().trim();
-
-    // Don't forget to provide initial data to your Firebase database. - set replaces old data
-    //if you want to add more users than just the latest one, then use push
-    //database.ref().set({
-    var trainInfo = {
-        formtrain: train,
-        formdestination: destination,
-        formfrequency: frequency,
-        formfirsttime: firstTime,
-        dateAdded: firebase.database.ServerValue.TIMESTAMP
+    // Initialize Firebase
+    
+    var config = {
+        apiKey: "AIzaSyCT9CQOSAH2X2HDvgfPgB2j_2cyURVMqhM",
+        authDomain: "trainscheduler-ad1ab.firebaseapp.com",
+        databaseURL: "https://trainscheduler-ad1ab.firebaseio.com",
+        projectId: "trainscheduler-ad1ab",
+        storageBucket: "trainscheduler-ad1ab.appspot.com",
+        messagingSenderId: "860681953163"
     };
-    //this is added so we can get most resent user so we can get most recent user to brower and to do this we need to change the listener  
-    database.ref().push(trainInfo);
+    firebase.initializeApp(config);
 
-    console.log(trainInfo.formtrain);
-    console.log(trainInfo.formdestination);
-    console.log(trainInfo.formfrequency);
-    console.log(trainInfo.formfirsttime);
-    console.log(trainInfo.dateAdded);
 
-    // alert("Train was successfully added");
+    //create a variable to reference the database
 
-    // Clears all of the text-boxes
-    $("#trainname-input").val("");
-    $("#destination-input").val("");
-    $("#frequency-input").val("");
-    $("#firsttime-input").val("");
+    var dataRef = firebase.database();
+
+    //on button click, store data
+
+    $("#submit-btn").on("click", function(event) {
+        //don't refresh the page
+        event.preventDefault();
+
+        //code in logic for storing and retrieving the most recent information.
+
+        var name = $("#name").val().trim();
+        var destination = $("#destination").val().trim();
+        var firstTrain = $("#firstTrain").val().trim();
+        var frequency = $("#frequency").val().trim();
+
+        //clear input fields after submit
+
+        $("#name").val("");
+        $("#destination").val("");
+        $("#firstTrain").val("");
+        $("#frequency").val("");
+
+        //push data rather than set in order to add onto previous data
+
+        dataRef.ref().push({
+            name: name,
+            destination: destination,
+            time: firstTrain,
+            frequency: frequency
+        });
+    });
+
+
+    //create firebase "watcher"
+
+    dataRef.ref().on("child_added", function(childSnapshot) {
+        console.log(childSnapshot.val());
+
+
+        //create new variables for clean build from childSnapshot of data from firebase
+
+        var name = childSnapshot.val().name;
+        var destination = childSnapshot.val().destination;
+        var frequency = childSnapshot.val().frequency;
+        var time = childSnapshot.val().time;
+        var key = childSnapshot.key;
+        var remove = "<button class='glyphicon glyphicon-trash' id=" + key + "></button>"
+
+
+        //code in math to find the next train time and minutes until next arrival based off of frequency value and first train time value.
+
+        //convert first train time back a year to make sure it is set before current time before pushing to firebase.
+
+        var firstTrainConverted = moment(time, "hh:mm").subtract(1, "years");
+        console.log(firstTrainConverted);
+
+        //set a variable equal to the current time from moment.js
+
+        var currentTime = moment();
+        console.log("Current Time: " + moment(currentTime).format("hh:mm"));
+
+        //post current time to jumbotron for reference
+
+        $("#currentTime").html("Current Time: " + moment(currentTime).format("hh:mm"));
+
+        //find the difference between the first train time and the current time
+
+        var timeDiff = moment().diff(moment(firstTrainConverted), "minutes");
+        console.log("Difference In Time: " + timeDiff);
+
+        //find the time apart by finding the remainder of the time difference and the frequency - use modal to get whole remainder number
+
+        var timeRemainder = timeDiff % frequency;
+        console.log(timeRemainder);
+
+        //find the minutes until the next train
+
+        var nextTrainMin = frequency - timeRemainder;
+        console.log("Minutes Till Train: " + nextTrainMin);
+
+        //find the time of the next train arrival
+
+        var nextTrainAdd = moment().add(nextTrainMin, "minutes");
+        var nextTrainArr = moment(nextTrainAdd).format("hh:mm");
+        console.log("Arrival Time: " + nextTrainArr);
+
+        //prepend all information for train data submitted by user
+
+        $("#schedule").prepend("<tr><td>" + name + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + nextTrainArr + "</td><td>" + nextTrainMin + "</td><td>" + remove + "</td></tr>");
+
+
+    }, function(err) {
+        console.log(err);
+    });
+
+               //on click command to delete key when user clicks the trash can gliphicon
+
+    $(document).on("click", ".glyphicon-trash", deleteTrain);
+
+    function deleteTrain() {
+        var deleteKey = $(this).attr("id");
+        //console.log($(this).attr("id"));
+        dataRef.ref().child(deleteKey).remove();
+
+        location.reload();
+
+    }
 
 });
-
-/ Firebase watcher + initial loader 
-database.ref().on("child_added", function(childSnapshot, prevChildKey) {  
-  var train = childSnapshot.val().formtrain;
-  var destination = childSnapshot.val().formdestination;
-  var frequency = childSnapshot.val().formfrequency;
-  var firstTime = childSnapshot.val().formfirsttime;
-
-  // First Time (pushed back 1 year to make sure it comes before current time)
-  var firstTimeConverted = moment(firstTime, "hh:mm").subtract(1, "years");
-  console.log(firstTimeConverted);
-
-
-   //determine Current Time
-   var currentTime = moment();
-   console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm a"));
- 
-   //get timer functioning
-   $("#timer").text(currentTime.format("hh:mm a"));
- 
-   // Difference between the times
-   var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-   console.log("DIFFERENCE IN TIME: " + diffTime);
- 
